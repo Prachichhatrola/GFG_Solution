6a7ea8339c3d6574830e676dcbac020d0f320101
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-08-14 19:23:24 +0530

    Create Third_largest_element.java

---
+++ DSA_in_Java/Third_largest_element.java
@@ -0,0 +1,29 @@
+class Third_largest_element
+{
+    int thirdLargest(int arr[], int n)
+    {
+	    // Your code here
+	    int max=Integer.MIN_VALUE;
+	    int count=0;
+	    while(count!=3){
+	        for(int i=0;i<n; i++){
+	            if(arr[i]>max){
+	                max=arr[i];
+	            }
+	        }
+	        if(count==2){
+	            break;
+	        }
+	        for(int i=0;i<n;i++){
+	            if(arr[i]==max){
+	                arr[i]=0;
+	                max=Integer.MIN_VALUE;
+	                count++;
+	                break;
+	            }
+	        }
+	    }
+	    return max;
+	    
+    }
+}
