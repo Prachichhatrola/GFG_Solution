35445b5a90278f638710cc14f890b7d690bcb1cb
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-10-23 18:24:55 +0530

    Create Implement_Stack_using_Linked_List.java

---
+++ DSA_in_Java/Implement_Stack_using_Linked_List.java
@@ -0,0 +1,37 @@
+class MyStack 
+{
+    // class StackNode {
+    //     int data;
+    //     StackNode next;
+    //     StackNode(int a) {
+    //         data = a;
+    //         next = null;
+    //     }
+    // }   
+    StackNode top;
+    
+    //Function to push an integer into the stack.
+    void push(int a) 
+    {
+        // Add your code here
+        StackNode node=new StackNode(a);
+        if(top==null){
+            top=node;
+            return;
+        }
+        node.next=top;
+        top=node;
+    }
+    
+    //Function to remove an item from top of the stack.
+    int pop() 
+    {
+        // Add your code here
+        if(top==null){
+            return -1;
+        }
+        int t=top.data;
+        top=top.next;
+        return t;
+    }
+}
