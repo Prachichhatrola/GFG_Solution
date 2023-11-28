9f1f66c3d22235ed16f214dc29dad526c976b07b
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-11-28 07:30:29 +0530

    Create Binary_Tree_to_DLL.java

---
+++ DSA_in_Java/Binary_Tree_to_DLL.java
@@ -0,0 +1,14 @@
+class Solution
+{
+    Node prev;
+    Node bToDLL(Node root)
+    {
+       if(root == null) return root;
+       Node head = bToDLL(root.left);
+       if(prev == null) head = root;
+       else            { prev.right = root; root.left = prev;}
+       prev = root;
+       bToDLL(root.right);
+      return head;
+    }
+}
