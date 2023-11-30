1354143fc1eccd96ff40e83e74985c3e41ec7da6
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-11-30 08:15:03 +0530

    Create Shortest_path_from_1_to_n.java

---
+++ DSA_in_Java/Shortest_path_from_1_to_n.java
@@ -0,0 +1,18 @@
+class Solution{
+    public int minStep(int n)
+    {
+        // code here
+         int ans=0;
+       while(n>1)
+       {
+           if(n%3==0)
+           {
+           n=n/3;
+           }
+           
+               else n--;
+           ans++;
+       }
+       return ans;
+    }
+}
