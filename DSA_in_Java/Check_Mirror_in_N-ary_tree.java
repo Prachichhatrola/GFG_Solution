275840b0abc22067c4af4f9ddba18469819f71cd
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-12-18 08:07:51 +0530

    Create Check_Mirror_in_N-ary_tree.java

---
+++ DSA_in_Java/Check_Mirror_in_N-ary_tree.java
@@ -0,0 +1,19 @@
+class Solution {
+    static int checkMirrorTree(int n, int e, int[] A, int[] B) {
+        HashMap<Integer,Stack<Integer>> map = new HashMap<>();
+        
+        for(int i=0; i<2*e; i+=2) {
+            if(!map.containsKey(A[i])) {
+               map.put(A[i], new Stack<>());
+            }
+            map.get(A[i]).push(A[i+1]);
+        }
+        
+        for(int i=0; i<2*e; i+=2) {
+            if (map.get(B[i]).peek() != B[i+1]) return 0;
+            map.get(B[i]).pop();
+        }
+        
+        return 1;
+    }
+};
