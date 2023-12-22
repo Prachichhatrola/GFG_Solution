4ee00da7b1af17b3babe8a3662b5ee694bf6d5de
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-12-22 11:02:54 +0530

    Update Diameter_of_a_Binary_Tree.java

--- DSA_in_Java/Diameter_of_a_Binary_Tree.java
+++ DSA_in_Java/Diameter_of_a_Binary_Tree.java
@@ -1,17 +1,17 @@
 class Solution {
+    int ans= 0  ; 
+    int height( Node root){
+        if(root == null) return 0;
+        
+        int lh = height( root.left);
+        int rh = height( root.right);
+        
+        ans =  Math.max ( ans , 1+lh+ rh);
+        return 1+ Math.max(lh , rh);
+    }
     // Function to return the diameter of a Binary Tree.
     int diameter(Node root) {
-        int[] dia = new int[1];
-        height(root,dia);
-        return dia[0];
-    }
-    int height(Node root,int[] dia) {
-        if(root == null)
-            return 0;
-        int lf = height(root.left,dia);
-        int rt = height(root.right,dia);
-        dia[0] = Math.max(dia[0],lf+rt+1);
-        return 1+Math.max(lf,rt);
+        int data   = height (root);
+        return ans;
     }
 }
-
