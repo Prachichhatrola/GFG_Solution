477e15e5369c457843fea50d5dc3d49584fb2923
Prachi Chhatrola <prachichhatrola009@gmail.com>
2024-01-06 17:42:02 +0530

    Create minimum_Depth_of_a_Binary_Tree.java

---
+++ DSA_in_Java/minimum_Depth_of_a_Binary_Tree.java
@@ -0,0 +1,21 @@
+class Solution{
+    int ans = Integer.MAX_VALUE;
+    void Level(Node root,int mylevel){
+        if(root == null){
+            return;
+        }
+        if(root.left == null && root.right == null){
+            ans = Math.min(ans,mylevel);
+        }
+        Level(root.left,mylevel+1);
+        Level(root.right,mylevel+1);
+        
+    }
+    int minDepth(Node root){
+       Level(root,1);
+       if(ans == Integer.MAX_VALUE){
+           return 0;
+       }
+       return ans;
+    }
+}
