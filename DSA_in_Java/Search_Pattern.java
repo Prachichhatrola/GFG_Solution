cad6d62852cc02013898a8408e5ae880f86589c4
Prachi Chhatrola <prachichhatrola009@gmail.com>
2024-01-09 11:42:03 +0530

    Create Search_Pattern.java

---
+++ DSA_in_Java/Search_Pattern.java
@@ -0,0 +1,21 @@
+class Solution
+{
+    ArrayList<Integer> search(String pat, String txt)
+    {
+        ArrayList<Integer> al = new ArrayList<>();
+        int i = 0;
+        
+        while(true){
+            int idx = txt.indexOf(pat, i);
+            if(idx==-1){
+                break;
+            }else{
+                al.add(idx+1);
+                i=idx+1;
+            }
+        }
+        
+        if(al.size()==0)al.add(-1);
+        return al;
+    }
+}
