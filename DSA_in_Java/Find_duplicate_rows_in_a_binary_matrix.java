f67188a1429c5718e28e688f137c7602831415a1
Prachi Chhatrola <prachichhatrola009@gmail.com>
2024-01-14 08:36:44 +0530

    Create Find_duplicate_rows_in_a_binary_matrix.java

---
+++ DSA_in_Java/Find_duplicate_rows_in_a_binary_matrix.java
@@ -0,0 +1,22 @@
+class Solution
+{
+    public static ArrayList<Integer> repeatedRows(int matrix[][], int m, int n)
+    {
+        HashSet<List<Integer>> s = new HashSet<>();
+        ArrayList<Integer> res = new ArrayList<>();
+        
+        for(int i = 0; i < m; i++) 
+        {
+            List<Integer> l = new ArrayList<>();
+            for(int j = 0; j < n; j++)
+                l.add(matrix[i][j]);
+                
+            if(!s.contains(l))
+                s.add(l);
+            else
+                res.add(i);
+        }
+        
+        return res;
+    }
+}
