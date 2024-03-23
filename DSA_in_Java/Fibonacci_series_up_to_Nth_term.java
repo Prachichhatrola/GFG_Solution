785970a582b4a56c993c925ed8d4308d43ed3c76
Prachi Chhatrola <prachichhatrola009@gmail.com>
2024-03-23 09:20:51 +0530

    Create Fibonacci_series_up_to_Nth_term.java

---
+++ DSA_in_Java/Fibonacci_series_up_to_Nth_term.java
@@ -0,0 +1,34 @@
+class Solution {
+
+ 
+
+    int[] Series(int n) {
+
+        // code here
+
+        int m[]=new int[n+1];
+
+        int a=0,b=1,c=1;
+
+        int i=0;
+
+        int mod=1000000007;
+
+        while(i<=n){
+
+            m[i++]=a;
+
+            a=b;
+
+            b=c;
+
+            c=(a+b)%mod;
+
+        }
+
+        return m;
+
+    }
+
+}
+
