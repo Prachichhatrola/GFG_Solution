dcf5e9bee2dea2cdb4b175df9f71a2abf73c21ee
Prachi Chhatrola <prachichhatrola009@gmail.com>
2024-04-11 09:30:19 +0530

    Create Gray_to_Binary_equivalent.java

---
+++ DSA_in_Java/Gray_to_Binary_equivalent.java
@@ -0,0 +1,9 @@
+class Solution{
+    public static int grayToBinary(int n) {
+        int binary = 0;
+        for (; n != 0; n >>= 1) {
+            binary ^= n;
+        }
+        return binary;
+    }
+}
