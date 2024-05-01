7fdccb702f613b7caf757de7d88389daf1c46e0c
Prachi Chhatrola <prachichhatrola009@gmail.com>
2024-05-01 08:44:41 +0530

    Create Arrange_Consonants_and_Vowels.java

---
+++ DSA_in_Java/Arrange_Consonants_and_Vowels.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isVowel(char ch) {
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
+    }
+    
+    public Node arrangeCV(Node head) {
+        Node con = new Node('a');
+        
+        Node conHead = con;
+        
+        Node vow = new Node('a');
+        
+        Node vowHead = vow;
+        
+        while (head != null) {
+            
+            if (isVowel(head.data)) {
+                vow.next = head;
+                vow = vow.next;
+            } else {
+                con.next = head;
+                con = con.next;
+            }
+            
+            head = head.next;
+        }
+        
+        con.next = null;
+        
+        vow.next = conHead.next;
+        
+        return vowHead.next;
+    }
+}
